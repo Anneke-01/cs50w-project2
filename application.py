ed7541470802502abdc3a510b6f2743f69b9bc62
anneke-01 <anneke_paulina11@hotmail.com>
2021-05-28 09:17:15 -0600

    Agregando joinroom

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-from flask import Flask, render_template, session, request, redirect, g
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, session, request, redirect, g,jsonify
+from flask_socketio import SocketIO, emit,join_room, leave_room
 from flask_session import Session
 from functools import wraps
 
@@ -11,26 +11,32 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+js =  { "users":[]}
+with open('data.json', 'w') as file:
+        json.dump(js,file,indent=4)
+
 socketio = SocketIO(app,manage_session=False)
 data = []
+user_profiles={}
 mensajes = {"general":[]}
 
-def login_required(f):
-    """
-    Decorate routes to require login.
 
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-    """
+def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("username") is None:
-            return redirect("/chat")
+            return redirect("/")
         return f(*args, **kwargs)
     return decorated_function
+
+@app.route("/info")
+def info():
+    return mensajes
+
 @app.route('/',methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        session.clear();
+        
         username = request.form.get("username")
         if (username):
             with open('data.json', 'r') as file:
@@ -56,32 +62,93 @@ def index():
 @app.route('/chat',methods=["GET", "POST"])
 @login_required
 def chat():
-
-    return render_template("index.html")
+    canales = []
+    for i in mensajes:
+        canales.append(i)
+    return render_template("index.html", username = session["username"], canales = canales)
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
     print(session["username"])
-    with open('data.json', 'r') as file:
-        data = json.load(file)
-        data["users"].remove({"username":session["username"]})
-    with open('data.json', 'w') as file:
-        json.dump(data,file,indent=4)    
+    try:
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+            data["users"].remove({"username":session["username"]})
+        with open('data.json', 'w') as file:
+            json.dump(data,file,indent=4) 
+    except:
+        x = 1
+    session.clear()   
     return redirect("/")
+
+@app.route("/messages", methods=["GET", "POST"])
+def messages():
+    room = request.form.get("room")
+    dictcionario = {"Contador":1}
+    if not room or not mensajes.get(room):
+        dictcionario["Contador"]= 0
+        return jsonify(dictcionario)
+
+    dictcionario["items"] = mensajes[room]
+    dictcionario["Contador"] = 1
+    return jsonify(dictcionario) 
 
 @socketio.on("prueba")
 def msj(hala):
     print(hala)
+    
     respuesta = {
+        "room" : session.get("canal"),
        "username" : session.get("username"), # si se pone corchete, username no existe se da una excepción o key error. En cambio, si es entre paréntesis (get) devuelve none
        "mensaje" : hala["mensaje"]  # debe de ser igual que en el emit
     }
 
-    emit("recibido",respuesta)
+    if mensajes.get(session.get("canal")):
+        mensajes[session.get("canal")].append(hala)
+    else:
+        mensajes[session.get("canal")] = []
+        mensajes[session.get("canal")].append(hala)
 
 
+    emit("recibido",respuesta,room = respuesta["room"])
 
 
+@socketio.on('join')
+def on_join(data):
+    print(data)
+    room = data["room"]
+
+    session["canal"] = room
+    join_room(room)
+
+    msj = {
+        "username":"Admin",
+        "mensaje":session["username"] + ' has entered the room.'
+    }
+
+    emit("recibido",msj,room = session["canal"])
+
+@socketio.on('Crear')
+def crear(room):
+    r = room["room"]
+    if not mensajes.get(r):
+        mensajes["r"] = []
+        emit("agregarcanal", {"room":r},broadcast=True)
+
+
+@socketio.on('leave')
+def on_leave(data):
+
+    room = data["canal"]
+    
+    leave_room(room)
+    session["canal"] = ""
+
+    msj = {
+        "username":"Admin",
+        "mensaje":session["username"] + ' has left the room.'
+    }
+    emit("recibido",msj,room=room)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,15 +1,25 @@
 var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+socket.emit("join",{"room":localStorage.canal});
+refresh();
+
 socket.on("connection", data => {
-    alert("entraste wey :D");
+    console.log("esta entrando ");
+
 });
 
 document.querySelector("#message-form").onsubmit = () =>{
     let message = document.querySelector("#message-input").value;
-    console.log(message)
+    console.log(message);
     document.querySelector("#message-input").value = "";
 
-    socket.emit("prueba",{"mensaje": message});
+    var info = {
+        "mensaje": message,
+        "username": localStorage.username,
+        "room":localStorage.canal
+    };
+
+    socket.emit("prueba",info);
     return false;
 
 };
@@ -18,6 +28,71 @@ socket.on("recibido", data => {
     console.log(data);
     let li = document.createElement("li");
     li.textContent = data.mensaje + " " + data.username;
+    console.log(li);
     document.querySelector("#messagelist").append(li);
 });
 
+
+document.querySelector("#joinroom").onsubmit = () =>{
+    let room = document.querySelector("#room").value;
+    console.log(room)
+    document.querySelector("#room").value = "";
+    socket.emit("Crear",{"room":room});
+    socket.emit("leave",{"room":localStorage.canal});
+
+    socket.emit("join",{"room": room});
+    localStorage.canal = room;
+    refresh();
+    return false;
+};
+socket.on("agregarcanal", data =>{
+    var listacanales = document.querySelector("#canal");
+    var button = document.createElement("button");
+    let canal = data["room"];
+    button.setAttribute("value",canal);
+    button.textContent = "#" + canal;
+    let li = document.createElement("li");
+    li.appendChild(button);
+    listacanales.append(li);
+});
+
+socket.on("recibidoroom", data => {
+    console.log(data);
+
+});
+
+
+function refresh(){
+    var element = document.getElementById("messagelist");
+        while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    console.log("Tuve fe");
+    let data = new FormData();
+    let room = localStorage.canal;
+    console.log(room);
+    data.append("room",room);
+    let request = new XMLHttpRequest();
+    request.open("POST","/messages");
+    request.responseType = 'json';
+    request.send(data);
+    request.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            console.log("entra");
+            //console.log(request);
+            // Muestra la información obtenida en la petición
+            let resp = this.response;
+            console.log(resp.items); 
+            let mensajes = resp.items;
+            for(var i = 0; i <mensajes.length; i++){
+                let li = document.createElement("li");
+                li.setAttribute("class","justify-content: flex-end")
+                li.textContent = mensajes[i].mensaje + " " + mensajes[i].username; 
+                console.log(li);
+                
+                document.querySelector("#messagelist").append(li);
+            }
+        }
+    }
+
+}
